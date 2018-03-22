efeba165dc06b87724d84837607c54f44e76a672
Ihab Hussein <ihab@ihabhussein.com>
2018-03-22 08:24:58 +0200

    Skeleton registeration forms

--- src/accounts/site.py
+++ src/accounts/site.py
@@ -1,17 +1,17 @@
+from django.contrib.auth import views
 from django.urls import path
-from django.contrib.auth.views import *
 
 urlpatterns = [
-    path('login/', LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
 
-    path('password_change/', PasswordChangeView.as_view(), 'password_change'),
-    path('password_change_done/', PasswordChangeDoneView.as_view(), 'password_change_done'),
+    path('password_change/', views.PasswordChangeView.as_view(), name='password_change'),
+    path('password_change/done/', views.PasswordChangeDoneView.as_view(), name='password_change_done'),
 
-    path('password_reset/', PasswordResetView.as_view(), 'password_reset'),
-    path('password_reset_done/', PasswordResetDoneView.as_view(), 'password_reset_done'),
-    path('password_reset_confirm/', PasswordResetConfirmView.as_view(), 'password_reset_confirm'),
-    path('password_reset_complete/', PasswordResetCompleteView.as_view(), 'password_reset_complete'),
+    path('password_reset/', views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
 
 def get_urls():
