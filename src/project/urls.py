eee272195643e37a907b1cb477786a9ea638529e
Ihab Hussein <ihab@ihabhussein.com>
2018-04-19 12:24:45 +0200

    Fix typos

--- src/project/urls.py
+++ src/project/urls.py
@@ -1,10 +1,10 @@
 from django.conf import settings
-from django.conf.urls import url, include
+from django.urls import path, include
 from django.conf.urls.static import static
 from django.contrib import admin
 
 urlpatterns = [
-    pth('i18n/', include('django.conf.urls.i18n')),
-    pth('admin/', admin.site.urls),
+    path('i18n/', include('django.conf.urls.i18n')),
+    path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
