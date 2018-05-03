ab3ef2e00165caef12aa8cff17e600a297cd4e9b
Ihab Hussein <ihab@ihabhussein.com>
2018-05-03 15:56:01 +0200

    Write a PID file for FreeBSD

--- etc/bsd-rc.d
+++ etc/bsd-rc.d
@@ -9,10 +9,12 @@
 
 name=__NAME__
 command="gunicorn3 project.wsgi:application"
+pidfile="/var/run/__NAME__.pid"
+
 export PYTHONPATH="__CONF__:__PDIR__/src"
 export PYTHONUSERBASE="__PDIR__/.local"
 export DJANGO_SETTINGS_MODULE="__SAFE___django"
-export GUNICORN_CMD_ARGS="-b 127.0.0.1:__PORT__ -w 4 --reload --capture-output --error-logfile /var/log/__NAME__.err --access-logfile /var/log/__NAME__.log"
+export GUNICORN_CMD_ARGS="-b 127.0.0.1:__PORT__ -w 4 --reload -p /var/run/__NAME__.pid --capture-output --error-logfile /var/log/__NAME__.err --access-logfile /var/log/__NAME__.log"
 
 load_rc_config $name
 : ${__NAME___enable:=no}
