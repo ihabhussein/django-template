df0e2f309e77005d069f652cbfc588c02f8590fd
Ihab Hussein <ihab@ihabhussein.com>
2018-09-23 09:24:20 +0200

    Add Safari's well-know password change URL
    https://developer.apple.com/safari/technology-preview/release-notes/#r59

--- src/website/urls.py
+++ src/website/urls.py
@@ -2,9 +2,13 @@ from django.conf import settings
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.views.generic import RedirectView
+
 
 urlpatterns = [
-    path('i18n/', include('django.conf.urls.i18n')),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
+
+    path('/.well-known/change-password', RedirectView.as_view(url='password_change')),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
